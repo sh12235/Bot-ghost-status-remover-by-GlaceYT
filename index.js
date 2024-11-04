92e2798ccafa0b53631f934843482f676a10deeb
sh12235 <cxvxcvdcsdfvsdfdsfv@gmail.com>
2024-11-04 18:07:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["My dev: dana2 🎉", "Vibe & chill the best 👑"];
+const statusMessages = ["My dev: dana2 👩‍💻", "Vibe & chill the best 😃"];
 const statusTypes = [ 'dnd', 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
